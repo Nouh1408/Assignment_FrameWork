1694abd2a720ff0b03c8adffaf5038c80d4657e7
Nouh1408 <ahmedinouh@gmail.com>
2025-02-02 22:07:55 +0200

    ya rab

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,18 @@
 
 import React from 'react'
+import styles from './Home.module.css'
+import img_1 from '../../../src/assets/avataaars.png'
 
 export default function Home() {
   return (
+  <>
+  <div className={`d-flex justify-content-center align-items-center vh-100 ${styles.Home}`}>
     <div>
-      <h1>Home Page</h1>
+    <img src={img_1} className={styles.customWidth} alt="home" />
+    <h2>START FRAMEWORK</h2>
+
     </div>
+  </div>
+  </>
   )
 }
