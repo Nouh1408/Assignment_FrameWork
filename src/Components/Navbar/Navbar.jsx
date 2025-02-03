df0c09d3d3f68882d5a4a45620a6aaf71c3e6d2b
Nouh1408 <ahmedinouh@gmail.com>
2025-02-03 19:50:31 +0200

    yes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,20 +5,20 @@ export default function Navbar() {
   return (
    <nav className={`navbar navbar-expand-lg ${styles.bgnavbar} navbar-dark fixed-top`}>
   <div className="container">
-    <Link className="navbar-brand" to="">START FRAMEWORK</Link>
+    <Link className={`navbar-brand ${styles.logo}`} to="">START FRAMEWORK</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse " id="navbarSupportedContent">
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
         <li className="nav-item">
-          <NavLink className="nav-link " aria-current="page" to="about">About</NavLink>
+          <NavLink className={`nav-link ${styles.navBtn}`} aria-current="page" to="about">About</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link " aria-current="page" to="portfolio">Portfolio</NavLink>
+          <NavLink className={`nav-link ${styles.navBtn}`} aria-current="page" to="portfolio">Portfolio</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link " aria-current="page" to="contact">Contact</NavLink>
+          <NavLink className={`nav-link ${styles.navBtn}`} aria-current="page" to="contact">Contact</NavLink>
         </li>
       </ul>
     </div>
