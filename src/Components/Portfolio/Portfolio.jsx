d23e7c885b43a3d274ae96f60bde25d740bb07fb
Nouh1408 <ahmedinouh@gmail.com>
2025-02-04 16:52:44 +0200

    portfolio Progress

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import styles from './Portfolio.module.css'
+import house from '../../assets/poert1.png'
+import CommonStructure from '../commonStructure/commonStructure'
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <div className='text-center'>
+      <h1>PORTFOLIO COMPONENT</h1>
+      <CommonStructure/>
+      <div className="container mt-4">
+      <div className="row">
+        <div className="col-md-3">
+          <img className={styles.imgSize} src={house} alt="house" />
+        </div>
+      </div>
+      </div>
+    </div>
   )
 }
