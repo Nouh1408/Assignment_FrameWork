0529ce9b9c13936811df1f50ad7f3f692abdca02
Nouh1408 <ahmedinouh@gmail.com>
2025-02-04 16:42:09 +0200

    about complete

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import styles from './About.module.css'
+import CommonStructure from '../commonStructure/commonStructure'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="container  mt-5">
+      <div className='text-center text-light'>
+      <h1 className={styles.h1}>ABOUT COMPONENT</h1>
+      <CommonStructure/>
+      </div>
+      <div className="row text-light">
+        <div className="col-md-6">
+            <p className={styles.text}>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
+        </div>
+        <div className="col-md-6">
+            <p className={styles.text}>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
+        </div>
+      </div>
+    </div>
   )
 }
